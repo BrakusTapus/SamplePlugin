bef2cf02eb6d3c46e630cb35cc66b36b2033bee5
BrakusTapus <38063721+BrakusTapus@users.noreply.github.com>
2023-12-16 17:41:44 +0100

    more setup

--- SamplePlugin/Plugin.cs
+++ SamplePlugin/Plugin.cs
@@ -1,82 +1,100 @@
 using Dalamud.Game.Command;
-using Dalamud.IoC;
 using Dalamud.Plugin;
 using System.IO;
 using Dalamud.Interface.Windowing;
 using Dalamud.Plugin.Services;
 using SamplePlugin.Windows;
-using Dalamud.Logging;
 
-namespace SamplePlugin
+// Define the namespace for the plugin.
+namespace SamplePlugin;
+
+// Define the Plugin class which implements the IDalamudPlugin interface.
+// This interface is required for all Dalamud plugins.
+public sealed class Plugin : IDalamudPlugin
 {
-    public sealed class Plugin : IDalamudPlugin
+    // Name of the plugin, used by Dalamud to identify it.
+    public string Name => "Sample Plugin";
+
+    // A constant string to define the command name.
+    private const string CommandName = "/psp";
+
+    // Properties for various services and configurations.
+    // These are initialized via dependency injection in the constructor.
+    private DalamudPluginInterface PluginInterface { get; init; }
+
+    private ICommandManager CommandManager { get; init; }
+    private IPluginLog PluginLog { get; init; }
+
+    public Configuration Configuration { get; init; }
+    public WindowSystem WindowSystem = new("SamplePlugin");
+
+    // Windows for the plugin's UI.
+    private ConfigWindow ConfigWindow { get; init; }
+    private MainWindow MainWindow { get; init; }
+
+    // Constructor for the plugin.
+    // The [RequiredVersion("1.0")] attribute specifies the required version of the injected service.
+    public Plugin(DalamudPluginInterface pluginInterface, ICommandManager commandManager, IPluginLog pluginLog)
     {
-        public string Name => "Sample Plugin";
-        private const string CommandName = "/pmycommand";
+        // Initialize properties with the injected services.
+        this.PluginLog = pluginLog;
+        this.PluginInterface = pluginInterface;
+        this.CommandManager = commandManager;
 
-        private DalamudPluginInterface PluginInterface { get; init; }
-        private ICommandManager CommandManager { get; init; }
-        public Configuration Configuration { get; init; }
-        public WindowSystem WindowSystem = new("SamplePlugin");
+        // Log a debug message indicating the plugin is starting.
+        PluginLog.Debug($"Starting plugin -> [{Name}]");
 
-        private ConfigWindow ConfigWindow { get; init; }
-        private MainWindow MainWindow { get; init; }
+        // Load or create the plugin configuration.
+        this.Configuration = this.PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
 
-        public Plugin([RequiredVersion("1.0")] DalamudPluginInterface pluginInterface, [RequiredVersion("1.0")] ICommandManager commandManager)
+        // Initialize the configuration.
+        this.Configuration.Initialize(this.PluginInterface);
+
+        // you might normally want to embed resources and load them from the manifest stream
+
+        // Path 1 points to the build output path (ex: C:\Users\AJvdM\AppData\Roaming\XIVPlugins\Projects\SamplePlugin\SamplePlugin\bin\x64\Release)
+        // Path 2 points to Path 1 + "images\goat.png"
+        var imagePath = Path.Combine(PluginInterface.AssemblyLocation.Directory?.FullName!, "images\\icon.png");
+        var goatImage = this.PluginInterface.UiBuilder.LoadImage(imagePath);
+
+        ConfigWindow = new ConfigWindow(this);
+        MainWindow = new MainWindow(this, goatImage);
+
+        WindowSystem.AddWindow(ConfigWindow);
+        WindowSystem.AddWindow(MainWindow);
+
+        this.CommandManager.AddHandler(CommandName, new CommandInfo(OnCommand)
         {
-            PluginLog.Debug($"Starting plugin -> [{Name}]");
-            this.PluginInterface = pluginInterface;
-            this.CommandManager = commandManager;
+            HelpMessage = "A useful message to display in /xlhelp"
+        });
 
-            this.Configuration = this.PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
-            this.Configuration.Initialize(this.PluginInterface);
+        this.PluginInterface.UiBuilder.Draw += DrawUI;
+        this.PluginInterface.UiBuilder.OpenConfigUi += DrawConfigUI;
+    }
 
-            // you might normally want to embed resources and load them from the manifest stream
+    public void Dispose()
+    {
+        this.WindowSystem.RemoveAllWindows();
 
-            // Path 1 points to the build output path (ex: C:\Users\AJvdM\AppData\Roaming\XIVPlugins\Projects\SamplePlugin\SamplePlugin\bin\x64\Release)
-            // Path 2 points to Path 1 + "images\goat.png"
-            var imagePath = Path.Combine(PluginInterface.AssemblyLocation.Directory?.FullName!, "images\\icon.png");
-            var goatImage = this.PluginInterface.UiBuilder.LoadImage(imagePath);
-            
-            ConfigWindow = new ConfigWindow(this);
-            MainWindow = new MainWindow(this, goatImage);
+        ConfigWindow.Dispose();
+        MainWindow.Dispose();
 
-            WindowSystem.AddWindow(ConfigWindow);
-            WindowSystem.AddWindow(MainWindow);
+        this.CommandManager.RemoveHandler(CommandName);
+    }
 
-            this.CommandManager.AddHandler(CommandName, new CommandInfo(OnCommand)
-            {
-                HelpMessage = "A useful message to display in /xlhelp"
-            });
+    private void OnCommand(string command, string args)
+    {
+        // in response to the slash command, just display our main ui
+        MainWindow.IsOpen = true;
+    }
 
-            this.PluginInterface.UiBuilder.Draw += DrawUI;
-            this.PluginInterface.UiBuilder.OpenConfigUi += DrawConfigUI;
-        }
+    private void DrawUI()
+    {
+        this.WindowSystem.Draw();
+    }
 
-        public void Dispose()
-        {
-            this.WindowSystem.RemoveAllWindows();
-
-            ConfigWindow.Dispose();
-            MainWindow.Dispose();
-
-            this.CommandManager.RemoveHandler(CommandName);
-        }
-
-        private void OnCommand(string command, string args)
-        {
-            // in response to the slash command, just display our main ui
-            MainWindow.IsOpen = true;
-        }
-
-        private void DrawUI()
-        {
-            this.WindowSystem.Draw();
-        }
-
-        public void DrawConfigUI()
-        {
-            ConfigWindow.IsOpen = true;
-        }
+    public void DrawConfigUI()
+    {
+        ConfigWindow.IsOpen = true;
     }
 }
